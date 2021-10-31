9387b7ed02b37e3c3ef7d7b46546eb9fb2ab471f
kim <82406962+KristinaKim6686@users.noreply.github.com>
2021-10-31 10:50:37 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,45 @@
+
+import * as _ from "lodash";
+import * as PNotify from "@pnotify/core";
+import * as PNotifyMobile from "@pnotify/mobile";
+import "@pnotify/core/dist/BrightTheme.css";
+import fetchCountry from './templates/js/fetch-countries-api';
+import countryCardTpl from './templates/hbs/country-cards.hbs';
+import countryList from './templates/hbs/country-list.hbs';
+import './templates/styles.css'
+
+const refs = {
+    countryCardContainer: document.querySelector('.country__data'),
+    searchInput: document.querySelector('.input__control')
+};
+
+const myStack = new PNotify.Stack({
+  dir1: "up",
+});
+refs.searchInput.addEventListener('input', _.debounce(onSearch,500));
+
+
+function onSearch(e) {
+  e.preventDefault();
+    fetchCountry(e).then((data) => {
+      refs.countryCardContainer.innerHTML = "";
+      myStack.close(true);
+      if (data.length === 1) {
+            refs.countryCardContainer.insertAdjacentHTML("beforeend",countryCardTpl(data[0]) )
+      } else if (data.length<11) {
+        data.forEach((country )=>{
+          console.log(country.name);
+        });
+        refs.countryCardContainer.insertAdjacentHTML("beforeend",countryList(data))
+      } else {
+        PNotify.notice({
+          text: "Too many matches found. Please, enter a more specific name!",
+          stack: myStack,
+          modules:new Map([...PNotify.defaultModules, [PNotifyMobile, {}]]),
+        })
+    }
+ })
+  
+}
+
+
---
+++ src/templates/js/fetch-countries-api.js
@@ -0,0 +1,15 @@
+export default function fetchCountries(e) {
+    const form=document.querySelector("input")
+    const searchQuery = e.target.value;
+
+    const url = `https://restcountries.com/v2/name/${searchQuery}`;
+    return fetch(url)
+        .then(
+            response =>
+                response.json())
+        .then(
+            data => data)
+        .finally(
+        form.reset
+    )
+}
