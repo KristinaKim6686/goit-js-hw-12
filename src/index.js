80c3cec03ab17d8a23fd44014624b94c0fa261fc
kim <82406962+KristinaKim6686@users.noreply.github.com>
2021-11-01 21:09:05 +0300

    catch fixed

--- src/index.js
+++ src/index.js
@@ -10,8 +10,9 @@ import './templates/styles.css'
 
 const refs = {
     countryCardContainer: document.querySelector('.country__data'),
-    searchInput: document.querySelector('.input__control')
+  searchInput: document.querySelector('.input__control')
 };
+const error = new Error;
 
 const myStack = new PNotify.Stack({
   dir1: "up",
@@ -22,31 +23,32 @@ refs.searchInput.addEventListener('input', _.debounce(onSearch,500));
 function onSearch(e) {
   e.preventDefault();
   fetchCountry(e)
+    .then(data => data.ok ? data : Promise.reject(data))
     .then((data) => {
-    refs.countryCardContainer.innerHTML = "";
-    myStack.close(true);
-    if (data.length === 1) {
-      refs.countryCardContainer.insertAdjacentHTML("beforeend", countryCardTpl(data[0]))
-    } else if (data.length < 11 ) {
-      data.forEach((country) => {
-        console.log(country.name);
-      });
-      refs.countryCardContainer.insertAdjacentHTML("beforeend", countryList(data))
-    } else if(data.length>10 ){
-      PNotify.notice({
-        text: "Too many matches found. Please, enter a more specific name!",
-        stack: myStack,
-        modules: new Map([...PNotify.defaultModules, [PNotifyMobile, {}]]),
-      })
-    } else {
-       PNotify.notice({
+      refs.countryCardContainer.innerHTML = "";
+      myStack.close(true);
+      if (data.length === 1) {
+        refs.countryCardContainer.insertAdjacentHTML("beforeend", countryCardTpl(data[0]))
+      } else if (data.length < 11) {
+        data.forEach((country) => {
+          console.log(country.name);
+        });
+        refs.countryCardContainer.insertAdjacentHTML("beforeend", countryList(data))
+      } else if (data.length > 10) {
+        PNotify.notice({
+          text: "Too many matches found. Please, enter a more specific name!",
+          stack: myStack,
+          modules: new Map([...PNotify.defaultModules, [PNotifyMobile, {}]]),
+        })
+      }
+    })
+    .catch(onError);
+}
+
+function onError(error){
+  PNotify.notice({
         text: "Such country doesn't exist!Try to type something normal O.o",
         stack: myStack,
         modules: new Map([...PNotify.defaultModules, [PNotifyMobile, {}]]),
       })
-    }
-  })
-    
 }
-
-
--- src/templates/js/fetch-countries-api.js
+++ src/templates/js/fetch-countries-api.js
@@ -9,6 +9,10 @@ export default function fetchCountries(e) {
                 response.json())
         .then(
             data => data)
+        .catch(error => {
+
+            alert("dcsbhjbvdjfkzb ")
+        })
         .finally(
         form.reset
     )
